e44f1a90a68cfa1f5a31d325a834217c455786ad
HiamPTT <118176243+HiamPTT@users.noreply.github.com>
2023-08-30 20:18:18 +0700

    Update fullport.sh

--- fullport.sh
+++ fullport.sh
@@ -105,66 +105,29 @@ error_detect_depends() {
 
 # Pre-installation settings
 pre_install_docker_compose() {
-  read -p "Web đang sử dụng:" api_host
-  echo "--------------------------------"
-  echo "Bạn đã chọn ${api_host}"
-  echo "--------------------------------"
-  
-   #giới hạn thiết bị
-  read -p "Giới hạn thiết bị :" DeviceLimit
-  [ -z "${DeviceLimit}" ] && DeviceLimit="0"
-  echo "-------------------------------"
-  echo "Thiết bị tối đa là: ${DeviceLimit}"
-  echo "-------------------------------"
-  
-  #Node_ID
-  read -p " ID nút 80 (Node_ID):" node_id
-  echo "-------------------------------"
-  echo -e "Node_ID 80: ${node_id}"
-  echo "-------------------------------"
-  
-  read -p " ID nút 443 (Node_ID):" node_id1
-  echo "-------------------------------"
-  echo -e "Node_ID 443: ${node_id1}"
-  echo "-------------------------------"
-  
-  #Giao thức kết nối
-  echo -e "[1] V2ray"
-  echo -e "[2] Trojan"
-  echo -e "[3] Shadowsocks"
-  read -p "Chọn giao thức kết nối 443:" NodeType
-  if [ "$NodeType" == "1" ]; then
-    NodeType="V2ray"
-  elif [ "$NodeType" == "2" ]; then
-    NodeType="Trojan"
-  elif [ "$NodeType" == "3" ]; then
-    NodeType="Shadowsocks"
-  else 
-    NodeType="V2ray"
-  fi
-  echo "--------------------------------"
-  echo "Bạn đã chọn: ${NodeType}"
-  echo "--------------------------------"
-  
-  #CertDomain
-read -p "Nhập Domain 443:" CertDomain
-  [ -z "${CertDomain}" ] && CertDomain=".vpn4g.net"
-  echo "-------------------------------"
-  echo -e "Domain 443 là: ${CertDomain}"
-  echo "-------------------------------"
-  
-  #Cloudflare
-  read -p "Nhập Cloudflare_mail 443:" mail
-  [ -z "${mail}" ] && mail="0"
-  echo "-------------------------------"
-  echo -e "CLOUDFLARE_EMAIL là: ${mail}"
-  echo "-------------------------------"
-  
-  read -p "Nhập Cloudflare_key 443:" key
-  [ -z "${key}" ] && key="0"
-  echo "-------------------------------"
-  echo -e "CLOUDFLARE_KEY là: ${key}"
-  echo "-------------------------------"
+    read -p "Nhập Link Web :" domain
+    echo -e "Link Web là : ${domain}"
+
+    read -p "Nhập Api Key :" APIKEY
+    echo -e "API KEY là : ${APIKEY}"
+
+    read -p "Nhập Node ID port 80 :" node_80
+    echo -e "Node_80 là : ${node_80}"
+
+    read -p "Nhập địa chỉ IP port 80 :" ip_80
+    echo -e "Node_80 là : ${ip_80}"
+
+    read -p "Nhập Node ID port 443 :" node_443
+    echo -e "Node_443 là : ${node_443}"    
+
+    read -p "Nhập SpeedLimit :" SpeedLimit
+    echo -e "SpeedLimit = ${SpeedLimit}"
+
+    read -p "Nhập DeviceLimit :" DeviceLimit
+    echo -e "DeviceLimit = ${DeviceLimit}"
+
+    read -p "Nhập dns trên cloudflare node 443:" CertDomain443
+    echo -e "CertDomain443 = ${CertDomain443}"
   
 }
 
@@ -200,28 +163,29 @@ Log:
   Level: none # Log level: none, error, warning, info, debug 
   AccessPath: # /etc/XrayR/access.Log
   ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
-RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
-ConnetionConfig:
+DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/dns.html for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/routing.html for help
+InboundConfigPath: # /etc/XrayR/custom_inbound.json # Path to custom inbound config, check https://xtls.github.io/config/inbound.html for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/outbound.html for help
+ConnectionConfig:
   Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 10 # Connection idle time limit, Second
+  ConnIdle: 30 # Connection idle time limit, Second
   UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
   DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
+  BufferSize: 64 # The internal cache size of each connection, kB
 Nodes:
   -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    PanelType: "V2board" # Panel type: SSpanel, V2board, NewV2board, PMpanel, Proxypanel, V2RaySocks
     ApiConfig:
-      ApiHost: "https://${api_host}"
-      ApiKey: "chieclacuoicung123"
-      NodeID: ${node_id}
-      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
+      ApiHost: "${domain}"
+      ApiKey: "${APIKEY}"
+      NodeID: ${node_80}
+      NodeType: V2ray # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
       Timeout: 30 # Timeout for the api request
       EnableVless: false # Enable Vless for V2ray Type
       EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
+      SpeedLimit: ${SpeedLimit} # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: ${DeviceLimit} # Local settings will replace remote settings, 0 means disable
       RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
     ControllerConfig:
       ListenIP: 0.0.0.0 # IP address you want to listen
@@ -229,40 +193,50 @@ Nodes:
       UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
       EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
       DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
       EnableProxyProtocol: false # Only works for WebSocket and TCP
+      DisableSniffing: true # Disable domain sniffing 
+      AutoSpeedLimitConfig:
+        Limit: 0 # Warned speed. Set to 0 to disable AutoSpeedLimit (mbps)
+        WarnTimes: 0 # After (WarnTimes) consecutive warnings, the user will be limited. Set to 0 to punish overspeed user immediately.
+        LimitSpeed: 0 # The speedlimit of a limited user (unit: mbps)
+        LimitDuration: 0 # How many minutes will the limiting last (unit: minute)
+      GlobalDeviceLimitConfig:
+        Enable: false # Enable the global device limit of a user
+        RedisAddr: 127.0.0.1:6379 # The redis server address
+        RedisPassword: YOUR PASSWORD # Redis password
+        RedisDB: 0 # Redis DB
+        Timeout: 5 # Timeout for redis request
+        Expiry: 60 # Expiry time (second)
       EnableFallback: false # Only support for Trojan and Vless
       FallBackConfigs:  # Support multiple fallbacks
         -
           SNI: # TLS SNI(Server Name Indication), Empty for any
+          Alpn: # Alpn, Empty for any
           Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/features/fallback.html for details.
           ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
       CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "node1.test.com" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
+        CertMode: file # Option about how to get certificate: none, file, http, tls, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "${ip_80}" # Domain to cert
+        CertFile: /etc/XrayR/crt.pem # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/key.pem
         Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
         DNSEnv: # DNS ENV option used by DNS provider
           ALICLOUD_ACCESS_KEY: aaa
           ALICLOUD_SECRET_KEY: bbb
   -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    PanelType: "V2board" # Panel type: SSpanel, V2board, NewV2board, PMpanel, Proxypanel, V2RaySocks
     ApiConfig:
-      ApiHost: "https://${api_host}"
-      ApiKey: "chieclacuoicung123"
-      NodeID: ${node_id1}
-      NodeType: ${NodeType} # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
+      ApiHost: "${domain}"
+      ApiKey: "${APIKEY}"
+      NodeID: ${node_443}
+      NodeType: Trojan # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
       Timeout: 30 # Timeout for the api request
       EnableVless: false # Enable Vless for V2ray Type
       EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
+      SpeedLimit: ${SpeedLimit} # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: ${DeviceLimit} # Local settings will replace remote settings, 0 means disable
       RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
     ControllerConfig:
       ListenIP: 0.0.0.0 # IP address you want to listen
@@ -270,28 +244,64 @@ Nodes:
       UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
       EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
       DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
       EnableProxyProtocol: false # Only works for WebSocket and TCP
+      DisableSniffing: true # Disable domain sniffing 
+      AutoSpeedLimitConfig:
+        Limit: 0 # Warned speed. Set to 0 to disable AutoSpeedLimit (mbps)
+        WarnTimes: 0 # After (WarnTimes) consecutive warnings, the user will be limited. Set to 0 to punish overspeed user immediately.
+        LimitSpeed: 0 # The speedlimit of a limited user (unit: mbps)
+        LimitDuration: 0 # How many minutes will the limiting last (unit: minute)
+      GlobalDeviceLimitConfig:
+        Enable: false # Enable the global device limit of a user
+        RedisAddr: 127.0.0.1:6379 # The redis server address
+        RedisPassword: YOUR PASSWORD # Redis password
+        RedisDB: 0 # Redis DB
+        Timeout: 5 # Timeout for redis request
+        Expiry: 60 # Expiry time (second)
       EnableFallback: false # Only support for Trojan and Vless
       FallBackConfigs:  # Support multiple fallbacks
         -
           SNI: # TLS SNI(Server Name Indication), Empty for any
+          Alpn: # Alpn, Empty for any
           Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/features/fallback.html for details.
           ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
       CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "${CertDomain}" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
-        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        CertMode: file # Option about how to get certificate: none, file, http, tls, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "${CertDomain443}" # Domain to cert
+        CertFile: /etc/XrayR/crt.pem # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/key.pem
+        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
         DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: ${mail}
-          CLOUDFLARE_API_KEY: ${key}
+          ALICLOUD_ACCESS_KEY: aaa
+          ALICLOUD_SECRET_KEY: bbb
+  # -
+  #   PanelType: "NewV2board" # Panel type: SSpanel, V2board, NewV2board, PMpanel, Proxypanel, V2RaySocks
+  #   ApiConfig:
+  #     ApiHost: "http://127.0.0.1:668"
+  #     ApiKey: "123"
+  #     NodeID: 4
+  #     NodeType: Shadowsocks # Node type: V2ray, Shadowsocks, Trojan
+  #     Timeout: 30 # Timeout for the api request
+  #     EnableVless: false # Enable Vless for V2ray Type
+  #     EnableXTLS: false # Enable XTLS for V2ray and Trojan
+  #     SpeedLimit: 0 # Mbps, Local settings will replace remote settings
+  #     DeviceLimit: 0 # Local settings will replace remote settings
+  #   ControllerConfig:
+  #     ListenIP: 0.0.0.0 # IP address you want to listen
+  #     UpdatePeriodic: 10 # Time to update the nodeinfo, how many sec.
+  #     EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+  #     CertConfig:
+  #       CertMode: dns # Option about how to get certificate: none, file, http, dns
+  #       CertDomain: "node1.test.com" # Domain to cert
+  #       CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
+  #       KeyFile: /etc/XrayR/cert/node1.test.com.pem
+  #       Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+  #       Email: test@me.com
+  #       DNSEnv: # DNS ENV option used by DNS provider
+  #         ALICLOUD_ACCESS_KEY: aaa
+  #         ALICLOUD_SECRET_KEY: bbb
 EOF
   sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
 }
@@ -421,7 +431,7 @@ Install_xrayr() {
 # Initialization step
 clear
 while true; do
-  echo "Docker 80-443 XrayR for SHOPKIMOCHI.XYZ"
+  echo "Docker 80-443 XrayR for Xflash"
   echo "Modder: TÚN"
   echo "Vui lòng nhập một số để Thực Hiện Câu Lệnh:"
   for ((i = 1; i <= ${#operation[@]}; i++)); do
